b0036ab22cda19fcbda83572804aeb51af3661b2
watsontaka <139229993+watsontaka@users.noreply.github.com>
2023-09-24 18:40:11 +0900

    frist commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  
+  root to: 'items#index'
+  resources :items
 
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
