033f2325eb91a63f129a5d5b94f2084c72edd9f3
watsontaka <139229993+watsontaka@users.noreply.github.com>
2023-10-05 20:29:47 +0900

    バリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,13 @@ class User < ApplicationRecord
 
   with_options presence: true do
     validates :nickname
-    validates :password,       format: { with: /\A(?=.?[a-z])(?=.?\d)[a-z\d]+\z/i }
     validates :first_name,     format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
     validates :last_name,      format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
     validates :first_katakana, format: { with: /\A[ァ-ヶー－]+\z/ }
     validates :last_katakana,  format: { with: /\A[ァ-ヶー－]+\z/ }
     validates :birth_day
   end
+
+  validates :password,       format: { with: /\A(?=.?[a-z])(?=.?\d)[a-z\d]+\z/i }
+  
 end
