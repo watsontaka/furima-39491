f6f833586b3072f19ca0a4fe76b8652a5941330a
watsontaka <139229993+watsontaka@users.noreply.github.com>
2023-10-14 10:53:30 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :delete]
-  before_action :set_item,      only: [:show]
+  before_action :set_item,      only: [:show,:edit]
+  before_action :move_to_index,      only: [:edit]
 
   def index
     @items = Item.all.order(created_at: "DESC")
@@ -22,6 +23,19 @@ class ItemsController < ApplicationController
   def show
   end
 
+  def edit
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    if @item.save
+      redirect_to item_path(@item.id)
+    else
+      render :edit,status: :unprocessable_entity
+    end
+  end
+
   def delete
   end
 
@@ -33,6 +47,12 @@ class ItemsController < ApplicationController
 
   def set_item
     @item = Item.find(params[:id])
+  end
+
+  def move_to_index
+    unless @item.user_id == current_user.id
+      redirect_to action: :index
+    end
   end
 
 end
