e2ca35b4d20040e302f1ab1372791cabf62d0154
watsontaka <139229993+watsontaka@users.noreply.github.com>
2023-10-14 14:34:48 +0900

    コントローラー修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :delete]
-  before_action :set_item,      only: [:show,:edit]
+  before_action :set_item,      only: [:show,:edit,:update]
   before_action :move_to_index,      only: [:edit]
 
   def index
@@ -27,9 +27,7 @@ class ItemsController < ApplicationController
   end
 
   def update
-    @item = Item.find(params[:id])
-    @item.update(item_params)
-    if @item.save
+    if @item.update(item_params)
       redirect_to item_path(@item.id)
     else
       render :edit,status: :unprocessable_entity
